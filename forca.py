afd81758c01c3295cc98c520a8dab829afabe6b0
gabrielcastrodev <gcastrop5@gmail.com>
2018-08-22 17:48:49 -0300

    Atualização Forca

--- forca.py
+++ forca.py
@@ -7,18 +7,32 @@ def jogar():
 	enforcou = False
 	acertou = False
 
-	while(not enforcou and not acertou):
+	contador = 0
 
-		chute = input("Qual a letra? ")
+	while(not enforcou and not acertou and contador != 5):
+
+		chute = input("Qual a letra? ").strip()
 
 		index = 0
 		for letra in palavra_secreta:
-			if(chute == letra):
+			if(chute.lower() == letra):
 				print("Você acertou a letra: {}, na posição {}".format(chute, index))
-			index = index + 1			
+			index = index + 1
+
+		contador = contador + 1
 
 		print("")
 		print("Jogando...")
+		print("")
+
+		if(contador == 5):
+			reposta = input("Digite a Palavra Final: ").strip()
+			print("")
+			if(reposta == palavra_secreta):
+				print("Parabéns, a palavra secreta é {}".format(palavra_secreta))
+			else:
+				print("Você errou, volte depois!")
+				
 
 	print("Fim do jogo!")
 
